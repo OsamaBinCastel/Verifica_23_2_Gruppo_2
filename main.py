a42e0bbab22e1caebbd383e7ac05535567a79843
marcocolombi444 <119593538+marcocolombi444@users.noreply.github.com>
2023-02-23 13:10:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import colombi_marco_2
+import marco_colombi
 import mazzolena_alessandro_2
 
 def saluto_castellani_lorenzo_Gruppo_2():
     print("Buongiorno, Lorenzo Castellani")
 
 def main():
-    colombi_marco_2.saluto_colombi_marco_2()
+    marco_colombi.marco_colombi_2()
     mazzolena_alessandro_2.saluto_mazzolena_alessandro_2()
     saluto_castellani_lorenzo_Gruppo_2()
 
