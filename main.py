206c14194c8d3f721570758e74bab9584a875955
OsamaBinCastel <119595167+OsamaBinCastel@users.noreply.github.com>
2023-02-23 12:53:58 +0100

    main code

---
+++ main.py
@@ -0,0 +1,14 @@
+import colombi_marco_2
+import mazzolena_alessandro_2
+
+def saluto_castellani_lorenzo_Gruppo_2():
+    print("Buongiorno, Lorenzo Castellani")
+
+def main():
+    colombi_marco_2.saluto_colombi_marco_2()
+    mazzolena_alessandro_2.saluto_mazzolena_alessandro_2()
+    saluto_castellani_lorenzo_Gruppo_2()
+
+
+
+main()
